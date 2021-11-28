1df14fb5392904fe9652936b91880b3631685ddf
GithubCShark <ngocdam2k@gmail.com>
2021-11-28 18:26:50 +0700

    Add project files.

---
+++ ASM.Server/ASM.API/Controllers/MonAnController.cs
@@ -0,0 +1,27 @@
+﻿using ASM.Share.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonAnController : ControllerBase
+    {
+        private readonly IMonAnSvc _monanSvc;
+
+        public MonAnController(IMonAnSvc monanSvc)
+        {
+            _monanSvc = monanSvc;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MonAn>>> GetMonAns()
+        {
+            return await _monanSvc.GetMonAnAllAsync();
+        }
+    }
+}
